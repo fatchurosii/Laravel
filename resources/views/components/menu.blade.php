12b4d08abcd9ad96e3dbe97b3d3af33a25b00f22
fatchurrosii <watashifrdesu@yahoo.com>
2020-08-10 14:16:20 +0700

    First

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,8 @@
+<nav class="nav flex-column">
+
+@foreach ($list as $row)
+<a class="nav-link {{ $isActive($row['label']) ? 'active' : ''}}" href="#">
+{{ $row['label'] }}
+    </a>
+@endforeach
+</nav>
